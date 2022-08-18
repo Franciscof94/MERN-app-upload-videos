cbfe78255e74f8e7189b49cfd89033166c00dc36
francisco <fran94_ferraro@hotmail.com>
2022-08-17 23:38:39 -0300

    first commit

--- frontend/src/services/VideoService.ts
+++ frontend/src/services/VideoService.ts
@@ -1,22 +1,24 @@
 import axios from 'axios';
 import { Video } from '../interfaces/video';
 
+const HOST = process.env.REACT_APP_API || 'https://localhost:4000/api/videos';
+
 export const getVideos = async () => {
-    return await axios.get<Video[]>('http://localhost:4000/api/videos')
+    return await axios.get<Video[]>('https://mern-app-upload-videos.herokuapp.com/api/videos');
 }
 
 export const getVideo = async (id: string) => {
-    return await axios.get<Video>(`http://localhost:4000/api/videos/${id}`)
+    return await axios.get<Video>(`https://mern-app-upload-videos.herokuapp.com/api/videos/${id}`)
 }
 
 export const createVideo = async (video: Video) => {
-    return await axios.post<Video>('http://localhost:4000/api/videos', video)
+    return await axios.post<Video>('https://mern-app-upload-videos.herokuapp.com/api/videos', video)
 }
 
 export const updateVideo = async (id: string, video: Video) => {
-    return await axios.put(`http://localhost:4000/api/videos/${id}`, video)
+    return await axios.put(`https://mern-app-upload-videos.herokuapp.com/api/videos/${id}`, video)
 }
 
 export const deleteVideoById = async (id: string) => {
-    return await axios.delete(`http://localhost:4000/api/videos/${id}`)
+    return await axios.delete(`https://mern-app-upload-videos.herokuapp.com/api/videos/${id}`)
 }
